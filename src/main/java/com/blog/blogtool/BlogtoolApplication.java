9e1acf675c6acd6568b5e44c29f3e36a308eee04
leeheeweon <ggoddii@naver.com>
2022-12-04 09:17:41 +0900

    first commit

---
+++ src/main/java/com/blog/blogtool/BlogtoolApplication.java
@@ -0,0 +1,13 @@
+package com.blog.blogtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogtoolApplication.class, args);
+	}
+
+}
